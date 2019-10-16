8072d44332f8aa606791bf1b15166b4494ff0544
Nnamdi Iroham <niroham@yahoo.com>
2019-10-15 20:38:58 -0500

    finally called my switch function with my code to print out results

--- liri.js
+++ liri.js
@@ -12,31 +12,31 @@ var moment = require("moment");
 var action = process.argv[2];
 var value = process[3];
 
+function runSwitch() {
+    switch (action) {
+        case "concert-this":
+            concertThis();
+            break;
 
-switch (action) {
-    case "concert-this":
-        concertThis();
-        break;
+        case "spotify-this-song":
+            spotifyThisSong();
+            break;
 
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
+        case "movie-this":
+            movieThis();
+            break;
 
-    case "movie-this":
-        movieThis();
-        break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
 
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
+        default:
+            break;
 
-    default:
-        break;
-
-        console.log("this is not recognized");
-
+            console.log("this is not recognized");
+    }
 }
-
+//Function for Bands/Artists Info: Bands in Town
 function concertThis() {
     var artist = process.argv[3];
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
@@ -45,14 +45,16 @@ function concertThis() {
     //axios
     axios.get(queryURL).then(
             function (response) {
-                // console.log(response.data);
+                //console.log(response.data);
                 for (let i = 0; i < response.data.length; i++) {
-                    console.log("**********CONCERT INFO*********");
-                    console.log(response.data[i].venue.name);
-                    console.log(response.data[i].venue.city);
-                    console.log(response.data[i].venue.region);
-                    console.log(moment(response.data[i].datetime).format("L"));
+                    fs.appendFileSync("log.txt", "**********CONCERT INFO*********" + "\n");
+                    fs.appendFileSync("log.txt", response.data[i].venue.name + "\n");
+                    fs.appendFileSync("log.txt", response.data[i].venue.city + "\n");
+                    fs.appendFileSync("log.txt", response.data[i].venue.region + "\n");
+                    fs.appendFileSync("log.txt", moment(response.data[i].datetime).format("L") + "\n");
+                    fs.appendFileSync("log.txt", "*****************************\n");
                 }
+
             })
 
         .catch(function (error) {
@@ -75,7 +77,7 @@ function concertThis() {
 
 }
 
-//Funtion for Music Info: Spotify
+//Function for Music Info: Spotify
 function spotifyThisSong() {
     if (process.argv[3] === undefined) {
         process.argv[3] = "I want it that way"; //default Song
@@ -111,25 +113,75 @@ function spotifyThisSong() {
     );
 };
 
+//Function for Movies Info: Movie Titles
+function movieThis() {
+    var movieName = process.argv[3];
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
 
+    //axios
+    axios.get(queryURL).then(
+            function (response) {
+                console.log(response.data);
+                // for (var i = 0; i < response.data.length; i++) {
 
+                // }
+                let data = response.data;
+                let str = [
+                    "**********MOVIE INFO*********",
+                    data.Title,
+                    data.Year,
+                    data.Ratings[0].Value,
+                    data.Ratings[1].Value,
+                    data.Country,
+                    data.Language,
+                    data.Plot,
+                    data.Actor,
+                    "*****************************\n"
+                ].join("\n");
 
-
-
-
-
-
-
-
-
-
-function showSomeInfo() {
-    fs.readFile('random.txt', 'utf8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        var dataArr = data.split(',');
-        UserInputs(dataArr[0], dataArr[1]);
-    });
+                fs.appendFile("log.txt", str, function (err) {
+                    if (err) {
+                        console.log(err)
+                    };
+                });
+            })
+        // If the request is successful
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 }
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+
+        var dataArr = data.split(",");
+
+        console.log(dataArr);
+
+        action = data.split(",")[0];
+        value = data.split(",")[1];
+
+        runSwitch();
+    })
+}
+runSwitch();
